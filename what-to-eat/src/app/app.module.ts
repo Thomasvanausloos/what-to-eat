d7ba73a56afffe87946279949def388f349ac2c7
Thomasvanausloos <thomasvanausloos@gmail.com>
2018-06-22 06:07:35 +0200

    added setup-modules

--- what-to-eat/src/app/app.module.ts
+++ what-to-eat/src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {RecipesModule} from "./recipes/recipes.module";
+import {CoreModule} from "./core/core.module";
+import {SharedModule} from "./shared/shared.module";
 
+const EXTERNAL_MODULES = [
+    BrowserModule
+];
+
+const INTERNAL_MODULES= [
+  RecipesModule,
+  CoreModule,
+  SharedModule
+];
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    EXTERNAL_MODULES,
+    ...INTERNAL_MODULES
   ],
   providers: [],
   bootstrap: [AppComponent]
