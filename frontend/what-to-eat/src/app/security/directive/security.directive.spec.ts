33d3d65868ff37bd8eaf6b449d2ccdacc3d22888
TVanausloos <thomas.vanausloos@cronos.be>
2020-04-04 17:34:55 +0200

    clean start: angular + nodejs

--- frontend/what-to-eat/src/app/security/directive/security.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import {SecurityDirective} from "./security.directive";
-
-describe('SecurityDirective', () => {
-  it('should create an instance', () => {
-    // const directive = new SecurityDirective();
-    // expect(directive).toBeTruthy();
-  });
-});
